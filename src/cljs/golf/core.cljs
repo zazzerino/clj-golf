88641842589ff112c8dde1c51e1e18af01e7c4f8
Kyle Patterson <patterkyle@gmail.com>
2020-11-17 20:58:10 -0600

    websocket frontend

--- src/cljs/golf/core.cljs
+++ src/cljs/golf/core.cljs
@@ -2,7 +2,7 @@
   (:require
    [day8.re-frame.http-fx]
    [reagent.dom :as rdom]
-   [reagent.core :as r]
+   [reagent.core :as r :refer [atom]]
    [re-frame.core :as rf]
    [goog.events :as events]
    [goog.history.EventType :as HistoryEventType]
@@ -10,10 +10,35 @@
    [golf.ajax :as ajax]
    [golf.events]
    [golf.subs]
+   [golf.websockets :as ws]
    [reitit.core :as reitit]
    [reitit.frontend.easy :as rfe]
    [clojure.string :as string])
   (:import goog.History))
+
+(defonce messages (atom []))
+
+(defn message-list []
+  [:ul
+   [:h4 "message list"]
+   (for [[i message] (map-indexed vector @messages)]
+     ^{:key i}
+     [:li message])])
+
+(defn message-input []
+  (let [value (atom nil)]
+    (fn []
+      [:input.form-control
+       {:type :text
+        :placeholder "enter message here"
+        :value @value
+        :on-change #(reset! value (-> % .-target .-value))
+        :on-key-down #(when (= (.-keyCode %) 13)
+                        (ws/send-transit-msg! {:message @value})
+                        (reset! value nil))}])))
+
+(defn update-messages! [{:keys [message]}]
+  (swap! messages #(vec (take 10 (conj % message)))))
 
 (defn nav-link [uri title page]
   [:a.navbar-item
@@ -44,6 +69,8 @@
 (defn home-page []
   [:section.section>div.container>div.content
    [:p "hello home page"]
+   [message-list]
+   [message-input]
    #_(when-let [docs @(rf/subscribe [:docs])]
      [:div {:dangerouslySetInnerHTML {:__html (md->html docs)}}])])
 
@@ -79,4 +106,6 @@
 (defn init! []
   (start-router!)
   (ajax/load-interceptors!)
+  (ws/make-websocket! (str "ws://" (.-host js/location) "/ws")
+                      update-messages!)
   (mount-components))
