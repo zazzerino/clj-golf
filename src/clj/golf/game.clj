95f2f709baaff20f255a877e4346f90c7f4c0978
Kyle Patterson <patterkyle@gmail.com>
2020-11-20 13:30:45 -0600

    basic game functions

---
+++ src/clj/golf/game.clj
@@ -0,0 +1,83 @@
+(ns golf.game
+  (:require [clojure.spec.alpha :as spec]))
+
+(def ranks [:ace :2 :3 :4 :5 :6 :7 :8 :9 :10 :jack :queen :king])
+(def suits [:clubs :diamonds :hearts :spades])
+
+(spec/def ::rank (set ranks))
+(spec/def ::suit (set suits))
+(spec/def ::card (spec/keys :req-un [::rank ::suit]))
+(spec/def ::deck (spec/* ::card))
+
+(spec/def :player/id string?)
+(spec/def :player/name string?)
+(spec/def :player/hand (spec/* ::card))
+(spec/def ::player (spec/keys :req-un [:player/id :player/name :player/hand]))
+(spec/def ::players (spec/map-of :player/id ::player))
+
+(spec/def ::table-card (spec/nilable ::card))
+(spec/def ::game (spec/keys :req-un [::players ::deck ::table-card]))
+
+(defn make-deck []
+  (for [rank ranks
+        suit suits]
+    {:rank rank
+     :suit suit}))
+
+(defn deal-card [deck]
+  {:card (first deck)
+   :deck (rest deck)})
+
+(defn make-player [id name]
+  {:id id
+   :name name
+   :hand []})
+
+(defn make-game []
+  {:players {}
+   :deck (make-deck)
+   :table-card nil})
+
+(defn add-player [game player]
+  (update-in game [:players] conj {(:id player) player}))
+
+(defn add-players [game players]
+  (reduce (fn [game player]
+            (add-player game player))
+          game
+          players))
+
+(defn give-card [player card]
+  (update-in player [:hand] conj card))
+
+(defn deal-card-to-player [game player-id]
+  (let [{:keys [card deck]} (deal-card (:deck game))
+        player (-> (get (:players game) player-id)
+                   (give-card card))]
+    (-> (assoc-in game [:players player-id] player)
+        (assoc :deck deck))))
+
+(defn deal-starting-hands [game]
+  (let [hand-size 6]
+    (loop [cards-remaining (* hand-size (count (:players game)))
+           player-ids (cycle (keys (:players game)))
+           game game]
+      (if (zero? cards-remaining)
+        game
+        (recur (dec cards-remaining)
+               (rest player-ids)
+               (deal-card-to-player game (first player-ids)))))))
+
+(defn deal-table-card [game]
+  (let [{:keys [card deck]} (deal-card (:deck game))]
+    (assoc game :table-card card :deck deck)))
+
+(defn shuffle-deck [game]
+  (assoc-in game [:deck] (shuffle (:deck game))))
+
+(defn init-game [game players]
+  (-> game
+      (add-players players)
+      (shuffle-deck)
+      (deal-table-card)
+      (deal-starting-hands)))
---
+++ test/clj/golf/game_test.clj
@@ -0,0 +1,43 @@
+(ns golf.game-test
+  (:require [clojure.test :refer :all]
+            [clojure.spec.alpha :as spec]
+            [golf.game :refer :all :as game]))
+
+(deftest test-game
+  (testing "create deck"
+    (is (spec/valid? ::game/deck (make-deck)))
+    (is (= 52 (count (make-deck)))))
+
+  (testing "dealing cards from a deck"
+    (let [deck (make-deck)
+          {:keys [card deck]} (deal-card deck)]
+      (is (= card {:rank :ace :suit :clubs}))
+      (is (= 51 (count deck)))))
+
+  (testing "create player"
+    (is (spec/valid? ::game/player (make-player "abcd1234" "Bob"))))
+
+  (testing "add players to game"
+    (let [g (make-game)
+          p1 (make-player "1234" "Alice")
+          p2 (make-player "56789" "Bob")]
+      (is (spec/valid? ::game/game g))
+      (is (empty? (:players g)))
+      (is (nil? (:table-card g)))
+      (let [g (add-player g p1)]
+        (is (= 1 (count (:players g))))
+        (is (= p1 (get (:players g) "1234")))
+        (is (spec/valid? ::game/game g))
+        (let [g (add-player g p2)]
+          (is (= 2 (count (:players g))))
+          (is (= p2 (get (:players g) "56789")))))))
+
+  (testing "dealing cards to players"
+    (let [player-id "a1b2c3"
+          p (make-player player-id "Charlie")
+          g (-> (make-game)
+                (add-player p)
+                (deal-card-to-player player-id))]
+      (is (= 51 (count (:deck g))))
+      (is (spec/valid? :player/hand (get-in g [:players player-id :hand])))
+      (is (= {:rank :ace :suit :clubs} (first (get-in g [:players player-id :hand])))))))
