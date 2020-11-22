9282e435803206a68e678ef1f991311940ee8b22
Kyle Patterson <patterkyle@gmail.com>
2020-11-21 20:35:37 -0600

    load card textures

--- src/cljs/golf/draw.cljs
+++ src/cljs/golf/draw.cljs
@@ -1,5 +1,6 @@
 (ns golf.draw
   (:require ["pixi.js" :as pixi]
+            [clojure.string :as string]
             [reagent.core :as reagent]))
 
 (def game-state (reagent/atom {}))
@@ -14,6 +15,7 @@
 
 (def renderer (make-renderer {:width 400 :height 400}))
 (def stage (pixi/Container.))
+(def loader pixi/Loader.shared)
 
 (defn draw [renderer stage]
   (.render renderer stage))
@@ -22,10 +24,55 @@
   (-> (.getElementById js/document id)
       (.appendChild renderer.view)))
 
+(def card-files
+  ["img/cards/svg/1B.svg", "img/cards/svg/2J.svg", "img/cards/svg/4C.svg",
+   "img/cards/svg/5H.svg", "img/cards/svg/7C.svg", "img/cards/svg/8H.svg",
+   "img/cards/svg/AC.svg", "img/cards/svg/JH.svg", "img/cards/svg/QC.svg",
+   "img/cards/svg/TH.svg", "img/cards/svg/1J.svg", "img/cards/svg/2S.svg",
+   "img/cards/svg/4D.svg", "img/cards/svg/5S.svg", "img/cards/svg/7D.svg",
+   "img/cards/svg/8S.svg", "img/cards/svg/AD.svg", "img/cards/svg/JS.svg",
+   "img/cards/svg/QD.svg", "img/cards/svg/TS.svg", "img/cards/svg/2B.svg",
+   "img/cards/svg/3C.svg", "img/cards/svg/4H.svg", "img/cards/svg/6C.svg",
+   "img/cards/svg/7H.svg", "img/cards/svg/9C.svg", "img/cards/svg/AH.svg",
+   "img/cards/svg/KC.svg", "img/cards/svg/QH.svg", "img/cards/svg/2C.svg",
+   "img/cards/svg/3D.svg", "img/cards/svg/4S.svg", "img/cards/svg/6D.svg",
+   "img/cards/svg/7S.svg", "img/cards/svg/9D.svg", "img/cards/svg/AS.svg",
+   "img/cards/svg/KD.svg", "img/cards/svg/QS.svg", "img/cards/svg/2D.svg",
+   "img/cards/svg/3H.svg", "img/cards/svg/5C.svg", "img/cards/svg/6H.svg",
+   "img/cards/svg/8C.svg", "img/cards/svg/9H.svg", "img/cards/svg/JC.svg",
+   "img/cards/svg/KH.svg", "img/cards/svg/TC.svg", "img/cards/svg/2H.svg",
+   "img/cards/svg/3S.svg", "img/cards/svg/5D.svg", "img/cards/svg/6S.svg",
+   "img/cards/svg/8D.svg", "img/cards/svg/9S.svg", "img/cards/svg/JD.svg",
+   "img/cards/svg/KS.svg", "img/cards/svg/TD.svg"])
+
+(defn card-name [filename]
+  (-> (string/split filename "/")
+      last
+      (string/split ".")
+      first))
+
+(defn texture-loaded? [name]
+  (aget loader.resources name))
+
+(defn load-card-texture [filename]
+  (let [name (card-name filename)]
+    (if-not (texture-loaded? name)
+      (.add loader name filename))))
+
+(defn load-card-textures [loader]
+  (doseq [file card-files]
+    (load-card-texture file))
+  (.load loader))
+
+(defn init-graphics [loader]
+  (load-card-textures loader)
+  (-> loader.onComplete (.add #(println "textures loaded"))))
+
 (defn game-canvas []
   (let [id "game-canvas"]
     (reagent/create-class
       {:component-did-mount (fn []
+                              (init-graphics loader)
                               (draw renderer stage)
                               (attach-view id renderer)
                               (println "game-canvas mounted"))
