f5faa8ea66f9c59729f69075fc7e62a9dde7c9fd
Kyle Patterson <patterkyle@gmail.com>
2020-11-21 21:09:39 -0600

    draw a card

--- src/cljs/golf/draw.cljs
+++ src/cljs/golf/draw.cljs
@@ -5,24 +5,10 @@
 
 (def game-state (reagent/atom {}))
 
-(defn make-renderer
-  ([{:keys [width height background-color]
-     :or {width 256 height 256 background-color 0xDDDDDD}}]
-   (pixi/Renderer. #js{"width" width
-                       "height" height
-                       "backgroundColor" background-color}))
-  ([] (make-renderer {})))
-
-(def renderer (make-renderer {:width 400 :height 400}))
-(def stage (pixi/Container.))
-(def loader pixi/Loader.shared)
-
-(defn draw [renderer stage]
-  (.render renderer stage))
-
-(defn attach-view [id renderer]
-  (-> (.getElementById js/document id)
-      (.appendChild renderer.view)))
+(def card-width 240)
+(def card-height 336)
+(def card-scale-x 0.3)
+(def card-scale-y 0.3)
 
 (def card-files
   ["img/cards/svg/1B.svg", "img/cards/svg/2J.svg", "img/cards/svg/4C.svg",
@@ -44,6 +30,18 @@
    "img/cards/svg/3S.svg", "img/cards/svg/5D.svg", "img/cards/svg/6S.svg",
    "img/cards/svg/8D.svg", "img/cards/svg/9S.svg", "img/cards/svg/JD.svg",
    "img/cards/svg/KS.svg", "img/cards/svg/TD.svg"])
+
+(defn make-renderer
+  ([{:keys [width height background-color]
+     :or {width 256 height 256 background-color 0xDDDDDD}}]
+   (pixi/Renderer. #js{"width" width
+                       "height" height
+                       "backgroundColor" background-color}))
+  ([] (make-renderer {})))
+
+(def renderer (make-renderer {:width 400 :height 400}))
+(def stage (pixi/Container.))
+(def loader pixi/Loader.shared)
 
 (defn card-name [filename]
   (-> (string/split filename "/")
@@ -68,17 +66,36 @@
   (load-card-textures loader)
   (-> loader.onComplete (.add #(println "textures loaded"))))
 
+(defn attach-view [id renderer]
+  (-> (.getElementById js/document id)
+      (.appendChild renderer.view)))
+
+(defn get-texture [name]
+  (-> (aget loader.resources name) .-texture))
+
+(defn make-card-sprite [name]
+  (doto (pixi/Sprite. (get-texture name))
+    (-> .-scale (.set card-scale-x card-scale-y))))
+
+(defn draw-table-card [stage card-name]
+  (let [sprite (make-card-sprite card-name)]
+    (.addChild stage sprite)))
+
+(defn draw [id renderer stage]
+  (.addChild stage (make-card-sprite "1B"))
+  (.render renderer stage)
+  (attach-view id renderer))
+
+(init-graphics loader)
+
 (defn game-canvas []
   (let [id "game-canvas"]
     (reagent/create-class
       {:component-did-mount (fn []
-                              (init-graphics loader)
-                              (draw renderer stage)
-                              (attach-view id renderer)
+                              (draw id renderer stage)
                               (println "game-canvas mounted"))
        :component-did-update (fn []
-                               (draw renderer stage)
-                               (attach-view id renderer)
+                               (draw id renderer stage)
                                (println "game-canvas updated"))
        :reagent-render (fn []
                          @game-state
